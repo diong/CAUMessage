c82dc1e37457e92e8c2c2a2f446d42f26d484945
wuzx <wuzx@feiyu.com>
2016-03-21 16:33:55 +0800

    Initial commit

---
+++ Example/CAUMessage/CAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CAAppDelegate.h
+//  CAUMessage
+//
+//  Created by wuzx on 03/21/2016.
+//  Copyright (c) 2016 wuzx. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
